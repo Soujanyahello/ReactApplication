d98bf432499398d8e7a1751ce1e48e444dc0c461
Soujanyahello <dasarisoujanya509@gmail.com>
2024-01-11 09:37:00 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Hello from './components/hello';
+import Welcome from './components/Welcome';
+import Greet from './components/Greet';
 function App() {
   return (
     <div className="App">
-     HELLo
+    
+    <Greet name="bruce" heroname="Fighter"><p>his is children props</p></Greet>
+    <Greet name="clark" heroname="Superman"><button>Action</button></Greet>
+    <Greet name="Diana" heroname="wonderwomen"></Greet>
+    
     </div>
   );
 }
---
+++ src/components/hello.js
@@ -0,0 +1,11 @@
+import React  from "react";
+const Hello = () =>{
+  //  return(
+       // <div>
+          //  Hello Soujanya
+        //</div>
+   // )
+
+   return  React.createElement('div' ,{id:'hello'},React.createElement('h1','li','hello','hello'))
+}
+export default Hello
